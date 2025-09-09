cfc378dd8fc9af8de858310b29cdc90e027b0052
Ingrid <ingrirt@stud.ntnu.no>
2025-09-09 13:49:52 +0200

    files

---
+++ CINELDI_MV_reference_system-flexibility_course_NTNU_public/exercise_0_battery_in_the_reference_system.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2023-07-13
+
+@author: ivespe
+
+Intro script for warm-up exercise ("exercise 0") in specialization course module 
+"Flexibility in power grid operation and planning" at NTNU (TET4565/TET4575) 
+"""
+import pandapower
+# %% Dependencies
+import pandapower as pp
+import pandapower.plotting as pp_plotting
+import pandas as pd
+import os
+import load_scenarios as ls
+import load_profiles as lp
+import pandapower_read_csv as ppcsv
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import math
+
+
+
+
+# %% Define input data
+
+# Location of (processed) data set for CINELDI MV reference system
+# (to be replaced by your own local data folder)
+
+path_data_set         = r'C:\Users\Ingrid\PycharmProjects\TET4565_fordypningsemne\CINELDI_MV_reference_system_v_DATA_2023-03-06'
+
+filename_residential_fullpath = os.path.join(path_data_set,'time_series_IDs_primarily_residential.csv')
+filename_irregular_fullpath = os.path.join(path_data_set,'time_series_IDs_irregular.csv')      
+filename_load_data_fullpath = os.path.join(path_data_set,'load_data_CINELDI_MV_reference_system.csv')
+filename_load_mapping_fullpath = os.path.join(path_data_set,'mapping_loads_to_CINELDI_MV_reference_grid.csv')
+
+# %% Read pandapower network
+
+net = ppcsv.read_net_from_csv(path_data_set, baseMVA=10)
+
+########### MY CODE ##########
+
+# TASK 3:
+pp.create_load_from_cosphi(net, bus=95, sn_mva=1, cos_phi=0.95, mode="underexcited")
+
+# TASK 4:
+#pp.create_load(net, bus=94, p_mw=-1.5)
+
+# TASK 5:
+#pp.create_load(net, bus=94, p_mw=-0.93)
+
+# TASK 6:
+pp.create_load(net, bus=94, p_mw=-0.8, q_mvar=-0.2)
+
+
+###############################
+
+
+# %% Test running power flow with a peak load model
+# (i.e., all loads are assumed to be at their annual peak load simultaneously)
+
+pp.runpp(net,init='results',algorithm='bfsw')
+
+print("V min: ",net.res_bus.vm_pu.min())
+print('Total load demand in the system assuming a peak load model: ' + str(net.res_load['p_mw'].sum()) + ' MW')
+
+# %% Plot results of power flow calculations
+
+pp_plotting.pf_res_plotly(net)
+
+# %%
+
+
